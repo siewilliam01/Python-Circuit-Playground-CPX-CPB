181c91c7055dbe3c7398cb57871c54784ce07d37
siewilliam01 <siewilliam01@gmail.com>
2025-01-28 09:35:05 -0800

    Update accelerometer_print.py

--- accelerometer_print.py
+++ accelerometer_print.py
@@ -1,4 +1,5 @@
 import time
+import math
 from adafruit_circuitplayground import cp
 
 data_smoothing_scale = 100
@@ -8,6 +9,10 @@ while True:
     x, y, z = data_smoothing_scale*x, data_smoothing_scale*y, data_smoothing_scale*z
     x, y, z = int(x), int(y), int(z)
     x, y, z = x/data_smoothing_scale, y/data_smoothing_scale, z/data_smoothing_scale
-    print(x, y, z)
+    angle = math.atan2(-y, -x)
+    angle = 180 + angle*180/(math.pi)
+    magnitude = math.sqrt(x**2+y**2)
+    print(angle, magnitude)
 
     time.sleep(0.01)
+
