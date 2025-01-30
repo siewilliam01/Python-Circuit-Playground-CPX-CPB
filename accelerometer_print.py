129054306700cdd7b60beeb2f006df2cc6c2fe57
siewilliam01 <siewilliam01@gmail.com>
2025-01-30 10:23:07 -0800

    Update accelerometer_print.py

--- accelerometer_print.py
+++ accelerometer_print.py
@@ -1,13 +1,9 @@
 import time
 import math
 import board
-import neopixel
 from adafruit_circuitplayground import cp
 
 data_smoothing_scale = 100
-
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.2, auto_write=False)
-pixels[0] = (255, 0, 0)
 
 while True:
     x, y, z = cp.acceleration
@@ -17,11 +13,7 @@ while True:
     angle = math.atan2(-y, -x)
     angle = 180 + angle*180/(math.pi)
     magnitude = math.sqrt(x**2+y**2)
-    brightness = 10*magnitude
-    
-    
-    
     print(angle, magnitude)
 
-    time.sleep(0.1)
+    time.sleep(0.01)
 
