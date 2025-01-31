da64c2903c34dbdd2b883c9b5431361de9d38199
siewilliam01 <siewilliam01@gmail.com>
2025-01-31 09:38:39 -0800

    Update accelerometer_print.py

--- accelerometer_print.py
+++ accelerometer_print.py
@@ -11,16 +11,14 @@ while True:
     x, y, z = int(x), int(y), int(z)
     x, y, z = x/data_smoothing_scale, y/data_smoothing_scale, z/data_smoothing_scale
     angle = math.atan2(-y, -x)
-    angle = 270 + angle*180/(math.pi)
+    angle = 180 + angle*180/(math.pi)
     magnitude = math.sqrt(x**2+y**2)
-    
-    angle = int(angle/36)
+    print(angle, magnitude)
+    angle = int((90+angle)/36)
     angle = angle % 10
     cp.pixels[angle] = (5*magnitude, 0, 0)
     for i in range(10):
         if(i != angle):
             cp.pixels[i] = (0, 0, 0)            
-
-    print(angle, magnitude)
     time.sleep(0.01)
 
