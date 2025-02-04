05fd3c3387aa316fd17d5966bdffc0019c6bb2d1
siewilliam01 <siewilliam01@gmail.com>
2025-02-04 08:47:21 -0800

    Update accelerometer_print.py

--- accelerometer_print.py
+++ accelerometer_print.py
@@ -5,15 +5,19 @@ from adafruit_circuitplayground import cp
 while True:
     x, y, z = cp.acceleration
     x, y, z = float(x), float(y), float(z)
+    
     angle = math.atan2(-y, -x)
     angle = angle*180/(math.pi)
-    magnitude = math.sqrt(x**2+y**2)
-    print(angle, magnitude)
     angle = int((270+angle)/36)
     angle = angle % 10
+    
+    magnitude = math.sqrt(x**2+y**2)
+        
     cp.pixels[angle] = (5*magnitude, 0, 0)
+    
     for i in range(10):
         if(i != angle):
-            cp.pixels[i] = (0, 0, 0)            
+            cp.pixels[i] = (0, 0, 0)
+            
     time.sleep(0.01)
 
